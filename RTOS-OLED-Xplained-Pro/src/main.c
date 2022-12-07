0b5da203ad7107d1256d7f1702d8fff9bb2dbb05
Pedro <pedrosrvd@al.insper.edu.br>
2022-12-07 15:33:46 -0300

    av3 funcionando e corrigida pelo CORSI

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -18,6 +18,47 @@ void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq) ;
 #define TASK_OLED_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
 #define TASK_OLED_STACK_PRIORITY            (tskIDLE_PRIORITY)
 
+#define LED_1_PIO PIOA
+#define LED_1_PIO_ID ID_PIOA
+#define LED_1_IDX 0
+#define LED_1_IDX_MASK (1 << LED_1_IDX)
+
+#define LED_2_PIO PIOC
+#define LED_2_PIO_ID ID_PIOC
+#define LED_2_IDX 30
+#define LED_2_IDX_MASK (1 << LED_2_IDX)
+
+#define LED_3_PIO PIOB
+#define LED_3_PIO_ID ID_PIOB
+#define LED_3_IDX 2
+#define LED_3_IDX_MASK (1 << LED_3_IDX)
+
+
+#define BUT_11_PIO PIOD
+#define BUT_11_PIO_ID ID_PIOD
+#define BUT_11_IDX 30
+#define BUT_11_IDX_MASK (1u << BUT_11_IDX)
+
+
+#define BUT_1_PIO PIOD
+#define BUT_1_PIO_ID ID_PIOD
+#define BUT_1_IDX 28
+#define BUT_1_IDX_MASK (1u << BUT_1_IDX)
+
+#define BUT_2_PIO PIOA
+#define BUT_2_PIO_ID ID_PIOA
+#define BUT_2_IDX 19
+#define BUT_2_IDX_MASK (1u << BUT_2_IDX)
+
+#define BUT_3_PIO PIOC
+#define BUT_3_PIO_ID ID_PIOC
+#define BUT_3_IDX 31
+#define BUT_3_IDX_MASK (1u << BUT_3_IDX)
+
+
+QueueHandle_t xQueueBut;
+volatile int vigia = 0;
+
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
 extern void vApplicationTickHook(void);
@@ -41,15 +82,246 @@ extern void vApplicationMallocFailedHook(void) {
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
+void TC0_Handler(void) {
+	/**
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	* Isso é realizado pela leitura do status do periférico
+	**/
+	volatile uint32_t status = tc_get_status(TC0, 0);
+	pin_toggle(BUT_11_PIO,BUT_11_IDX_MASK);
+	
+	
+
+}
+
+
+void but1_callback(void) {
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	int i = 0;
+	vigia++;
+	xQueueSendFromISR(xQueueBut, &i, &xHigherPriorityTaskWoken);
+}
+
+void but2_callback(void) {
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	int i = 1;
+	vigia++;
+	xQueueSendFromISR(xQueueBut, &i, &xHigherPriorityTaskWoken);
+}
+
+void but3_callback(void) {
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	int i = 2;
+	vigia++;
+	xQueueSendFromISR(xQueueBut, &i, &xHigherPriorityTaskWoken);
+}
+
+
+
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
 static void task_game(void *pvParameters) {
 	gfx_mono_ssd1306_init();
-	gfx_mono_draw_string("Level: 0", 0, 0, &sysfont);
+	gfx_mono_draw_string("Level: 10", 0, 0, &sysfont);
+	int level = 0;
+	int sequencia[512];
+	char str[100];
+	
+	pmc_enable_periph_clk(LED_1_PIO_ID);
+	pmc_enable_periph_clk(LED_2_PIO_ID);
+	pmc_enable_periph_clk(LED_3_PIO_ID);
+	pmc_enable_periph_clk(BUT_11_PIO_ID);
+	pmc_enable_periph_clk(BUT_1_PIO_ID);
+	pmc_enable_periph_clk(BUT_2_PIO_ID);
+	pmc_enable_periph_clk(BUT_3_PIO_ID);
+
+	
+	
+	
+	pio_configure(LED_1_PIO, PIO_OUTPUT_0, LED_1_IDX_MASK, PIO_DEFAULT);
+	pio_configure(LED_2_PIO, PIO_OUTPUT_0, LED_2_IDX_MASK, PIO_DEFAULT);
+	pio_configure(LED_3_PIO, PIO_OUTPUT_0, LED_3_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT_11_PIO, PIO_OUTPUT_0, BUT_11_IDX_MASK, PIO_DEFAULT);
+	
+	
+	
+	pio_configure(BUT_1_PIO, PIO_INPUT, BUT_1_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+	pio_configure(BUT_2_PIO, PIO_INPUT, BUT_2_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+	pio_configure(BUT_3_PIO, PIO_INPUT, BUT_3_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+
+	pio_handler_set(BUT_1_PIO, BUT_1_PIO_ID, BUT_1_IDX_MASK, PIO_IT_FALL_EDGE,
+	but1_callback);
+	pio_handler_set(BUT_2_PIO, BUT_2_PIO_ID, BUT_2_IDX_MASK, PIO_IT_FALL_EDGE,
+	but3_callback);
+	pio_handler_set(BUT_3_PIO, BUT_3_PIO_ID, BUT_3_IDX_MASK, PIO_IT_FALL_EDGE,
+	but2_callback);
+
+	pio_enable_interrupt(BUT_1_PIO, BUT_1_IDX_MASK);
+	pio_enable_interrupt(BUT_2_PIO, BUT_2_IDX_MASK);
+	pio_enable_interrupt(BUT_3_PIO, BUT_3_IDX_MASK);
+
+	pio_get_interrupt_status(BUT_1_PIO);
+	pio_get_interrupt_status(BUT_2_PIO);
+	pio_get_interrupt_status(BUT_3_PIO);
+
+	NVIC_EnableIRQ(BUT_1_PIO_ID);
+	NVIC_SetPriority(BUT_1_PIO_ID, 4);
+
+	NVIC_EnableIRQ(BUT_2_PIO_ID);
+	NVIC_SetPriority(BUT_2_PIO_ID, 4);
+
+	NVIC_EnableIRQ(BUT_3_PIO_ID);
+	NVIC_SetPriority(BUT_3_PIO_ID, 4);
+	
+	pio_set(LED_1_PIO,LED_1_IDX_MASK);
+	pio_set(LED_2_PIO,LED_2_IDX_MASK);
+	pio_set(LED_3_PIO,LED_3_IDX_MASK);
+
 
 	for (;;)  {
+		sprintf(str, "level : %d  ", level);
+		gfx_mono_draw_string(str, 0, 0, &sysfont);
+		
+		int n = genius_get_sequence(level,sequencia);
+		for(int j=0;j<n;j++){
+			if(vigia > 0){
+				
+				gfx_mono_draw_string("perdeu   ", 0, 0, &sysfont);
+				level = 0;
+				vigia = 0;
+				break;
+			}
+			if(sequencia[j] == 0){
+				//printf("entrei");
+				TC_init(TC0, ID_TC0, 0, 1000);
+				tc_start(TC0, 0);
+				//printf("sai");
+				pio_clear(LED_1_PIO,LED_1_IDX_MASK);
+				//delay_ms(1000);
+				vTaskDelay(500);
+				pio_set(LED_1_PIO,LED_1_IDX_MASK);
+				tc_stop(TC0,0);
+				vTaskDelay(100);
+			}
+			else if(sequencia[j] == 1){
+				TC_init(TC0, ID_TC0, 0, 1500);
+				tc_start(TC0, 0);
+				pio_clear(LED_2_PIO,LED_2_IDX_MASK);
+				//delay_ms(1000);
+				vTaskDelay(500);
+				pio_set(LED_2_PIO,LED_2_IDX_MASK);
+				tc_stop(TC0,0);
+				vTaskDelay(100);
+			}
+			else if(sequencia[j] == 2){
+				TC_init(TC0, ID_TC0, 0, 2000);
+				tc_start(TC0, 0);
+				pio_clear(LED_3_PIO,LED_3_IDX_MASK);
+				//delay_ms(1000);
+				vTaskDelay(500);
+				pio_set(LED_3_PIO,LED_3_IDX_MASK);
+				tc_stop(TC0,0);
+				vTaskDelay(100);
+				
+			}
+		}
+		//printf("n:%d\n",n);
+		//printf("s0:%d\n",sequencia[0]);
+		//printf("s1:%d\n",sequencia[1]);
+		//printf("s2:%d\n",sequencia[2]);
+		//pd30
+		int tentando = 1;
+		int t;
+		int p = 0;
+		int contagem = 0;
+		int vetor[100];
+		
+		while(tentando){
+			if (xQueueReceive(xQueueBut, &t, (TickType_t) 1200)){
+				printf("apertado: %d\n",t);
+				
+				
+				
+				if(t == 0){
+					TC_init(TC0, ID_TC0, 0, 1000);
+					tc_start(TC0, 0);
+					pio_clear(LED_1_PIO,LED_1_IDX_MASK);
+					vTaskDelay(100);
+					pio_set(LED_1_PIO,LED_1_IDX_MASK);
+					tc_stop(TC0,0);
+				}
+				else if(t == 1){
+					TC_init(TC0, ID_TC0, 0, 1500);
+					tc_start(TC0, 0);
+					pio_clear(LED_2_PIO,LED_2_IDX_MASK);
+					vTaskDelay(100);
+					pio_set(LED_2_PIO,LED_2_IDX_MASK);
+					tc_stop(TC0,0);
+				}
+				else if(t == 2){
+					TC_init(TC0, ID_TC0, 0, 2000);
+					tc_start(TC0, 0);
+					pio_clear(LED_3_PIO,LED_3_IDX_MASK);
+					vTaskDelay(100);
+					pio_set(LED_3_PIO,LED_3_IDX_MASK);
+					tc_stop(TC0,0);
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				contagem++;
+				if(contagem>n){
+					vigia=0;
+					break;
+				}
+				if(t == sequencia[p]){
+					p++;
+					if(contagem == n){
+						tentando = 0;
+						vigia=0;
+						level++;
+						vTaskDelay(1000);
+					}
+					
+				}
+				else{
+					tentando = 0;
+					printf("sequncia: %d, t : %d, p: %d",sequencia[p],t,p);
+					gfx_mono_draw_string("perdeu   ", 0, 0, &sysfont);
+					vTaskDelay(500);
+					level = 0;
+					vigia=0;
+				
+					
+				}
+				
+				
+				
+				
+				
+			}
+			else{
+				tentando = 0;
+				gfx_mono_draw_string("demorou   ", 0, 0, &sysfont);
+				level = 0;
+				vigia=0;
+				vTaskDelay(500);
+				
+			}
+			
+		}
+	
 		
 	}
 }
@@ -118,6 +390,7 @@ int main(void) {
 
 	/* Initialize the console uart */
 	configure_console();
+	xQueueBut = xQueueCreate(32, sizeof(int) );
 
 	/* Create task to control oled */
 	if (xTaskCreate(task_game, "game", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
